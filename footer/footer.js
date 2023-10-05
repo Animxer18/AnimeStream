01072bb6360f5cb84671cac5862cfe04ec069095
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2023-10-05 10:48:34 +0800

    Update footer.js

--- footer/footer.js
+++ footer/footer.js
@@ -5,8 +5,8 @@ class Footer extends HTMLElement{
         <footer class="footer p-5" style="display: none;">
         <div class="content has-text-centered">
           <p>
-            <strong><a href="#">Anime Stream</a></strong> Stream for free without ads! The source code is by <a
-              target="_blank" href="https://github.com/dustin1818">Dustin</a>. Credits to consumet.org for the api.
+            <strong><a href="#">AnimXer</a></strong> Stream for free without ads! The source code is by <a
+              target="_blank" href="https://github.com/dustin1818">ANIMXER</a>. SUPPORT MY FACEBOOK PAGE
           </p>
         </div>
       </footer>
