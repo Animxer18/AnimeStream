287357fcd9b5126de140a0a1e0c0fb7c7011c495
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2023-10-05 10:54:34 +0800

    Update footer.js

--- footer/footer.js
+++ footer/footer.js
@@ -5,8 +5,8 @@ class Footer extends HTMLElement{
         <footer class="footer p-5" style="display: none;">
         <div class="content has-text-centered">
           <p>
-            <strong><a href="#">AnimXer</a></strong> Stream for free without ads! The source code is by <a
-              target="_blank" href="https://github.com/dustin1818">ANIMXER</a>. SUPPORT MY FACEBOOK PAGE
+            <strong><a href="#">AnimXer</a></strong> Stream for free without ads! By: t<a
+              target="_blank" href="https://www.facebook.com/profile.php?id=100069572806122">ANIMXER</a>. SUPPORT MY FACEBOOK PAGE
           </p>
         </div>
       </footer>
